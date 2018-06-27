33a0bcd5f8eb69700ecb36d54682c1748990d981
anirudhce <amirudhce@gmail.com>
2018-06-27 17:27:01 +0530

    working polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,28 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,Http404,HttpResponse
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Question
-from django.core.urlresolvers import reverse
+from django.template import loader
+from django.urls import reverse
 
 
 # Create your views here.
 
 def index(request):
-    latest_question_list = get_list_or_404(Question)
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
-
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
-
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -33,5 +39,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
